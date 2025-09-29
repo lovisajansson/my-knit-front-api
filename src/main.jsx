a868ae9498c34df885ebe6e6e1ae34db8443b7be
Lovisa Jansson <lovisa.jansson96@gmail.com>
2025-09-29 16:01:18 +0200

    add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import KnittingCalculator from "./KnittingCalculator";
-import "./style.css";
+
+// Import Bootstrap CSS here
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
